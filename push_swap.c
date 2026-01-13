48e9ab3b17ae33be823e75785a422e5b622c4b15
jdszzz <cvalejandrobg@gmail.com>
2026-01-13 01:47:06 +0100

    Lógica de argumentos

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: albelmon <albelmon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 19:39:55 by albelmon          #+#    #+#             */
-/*   Updated: 2026/01/12 20:44:51 by albelmon         ###   ########.fr       */
+/*   Updated: 2026/01/13 01:38:48 by albelmon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,6 @@
 
 int	push_swap(int argc, char **argv)
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
-
-	stack_a = NULL;
-	stack_b = NULL;
 	ft_check_input(argc, **argv);
 	return (0);
 }
@@ -27,24 +22,43 @@ int	push_swap(int argc, char **argv)
 int	ft_check_input(int argc, char **argv)
 {
 	int	i;
+	int	j;
+	int	n;
 	
+	j = 0;
 	i = 0;
-	if (argc != 2)
+	n = 0;
+	if (argc < 2)
 		return (write(2, "error\n", 6));
 	while (*argv)
 	{
-		if (*argv == ' ' || *argv == ft_isdigit(*argv))
+		while (j || argv[i][j] == ' ' || argv[i][j] == ft_isdigit(argv[i][j]))
 		{
-			if (*argv == ft_isdigit(*argv))
-				ft_add_to_stack(*argv);
-			i++;
+			if (argv[i][j] == ft_isdigit(argv[i][j]))
+				ft_add_to_stack(argv[i][j], n++);
+			j++;
 		}
-		else
+		i++;
+		if (!argv[i][j] == ' ' || !argv[i][j] == ft_isdigit(*argv))
+			return (write(2, "error\n", 6));
+	}
+
+
+	while (*argv)
+	{
+		while (argv)
+		if (!argv[i][j] == ' ' || !argv[i][j] == ft_isdigit(*argv))
 			return (write(2, "error\n", 6));
 	}
 }
 
-int	ft_add_to_stack(int argv)
+void	ft_add_to_stack(int argv, int i)
 {
-	
+	t_stack *stack_a;
+
+	stack_a = malloc(sizeof(t_stack));
+	if (!stack_a)
+		return (NULL);
+	stack_a->value = argv;
+	stack_a->index = i;
 }
